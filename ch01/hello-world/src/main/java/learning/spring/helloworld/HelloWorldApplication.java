8491f209de38b71f2768f38e6f34c0aeabeb74e6
sbrace <sbrace0822@gmail.com>
2023-02-23 22:53:11 +0800

    ch01 hello-world add hello world rest api

--- ch01/hello-world/src/main/java/learning/spring/helloworld/HelloWorldApplication.java
+++ ch01/hello-world/src/main/java/learning/spring/helloworld/HelloWorldApplication.java
@@ -2,12 +2,20 @@ package learning.spring.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HelloWorldApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
+    }
+
+    @RequestMapping("/helloWorld")
+    public String helloWorld() {
+        return "Hello World! Bravo Spring!";
     }
 
 }
