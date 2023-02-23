5390b4eb7b65a179bf8c5af082ca823d0e1560c0
sbrace <sbrace0822@gmail.com>
2023-02-23 23:54:46 +0800

    ch02 hello-world init

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # learning-spring-samples
 《学透Spring：从入门到项目实战》  
-Learning spring samples  
+Learning Spring Samples  
 ISBN: 9787115609113  
 练习集  
 - Java 11.0.17
---
+++ ch02/hello-world/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Hello hello = new Hello();
+        System.out.println(hello.hello());
+    }
+}
