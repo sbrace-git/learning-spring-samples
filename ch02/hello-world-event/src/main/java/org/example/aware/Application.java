6a2cce01087979ac27480b33a55892c8735b6fb8
sbrace <sbrace0822@gmail.com>
2023-02-25 23:34:48 +0800

    ch02 hello-world-event ApplicationContextAware

--- ch02/hello-world-event/src/main/java/org/example/aware/Application.java
+++ ch02/hello-world-event/src/main/java/org/example/aware/Application.java
@@ -1,7 +1,30 @@
 package org.example.aware;
 
-public class Application {
-    public static void main(String[] args) {
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
 
+@Component
+public class Application implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(Application.class);
+        Application bean = annotationConfigApplicationContext.getBean(Application.class);
+        Application bean1 = bean.getBean(Application.class);
+        System.out.println(bean);
+        System.out.println(bean1);
+    }
+
+    public <T> T getBean(Class<T> tClass) {
+        return this.applicationContext.getBean(tClass);
     }
 }
