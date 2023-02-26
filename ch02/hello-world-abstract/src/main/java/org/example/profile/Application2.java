ec534bf0b395b5f707d260bbb74ce8567c0e5e03
sbrace <sbrace0822@gmail.com>
2023-02-27 00:47:04 +0800

    ch02 hello-world-abstract profile environment variables

---
+++ ch02/hello-world-abstract/src/main/java/org/example/profile/Application2.java
@@ -0,0 +1,34 @@
+package org.example.profile;
+
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+@Configuration
+@ImportResource(value = {"classpath:beans.xml", "classpath:beans-prod.xml"})
+public class Application2 {
+    public static void main(String[] args) {
+        /*
+         * environment variables
+         * spring.profiles.active=dev
+         * spring.profiles.active=prod
+         */
+        AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext();annotationConfigApplicationContext.register(Application2.class);
+        annotationConfigApplicationContext.refresh();
+        Hello hello = annotationConfigApplicationContext.getBean("hello-1", Hello.class);
+        hello.hello();
+        Hello hello2 = annotationConfigApplicationContext.getBean("hello-2", Hello.class);
+        hello2.hello();
+    }
+
+    @Profile("dev")
+    @Bean("hello-2")
+    public Hello helloDev2() {
+        return new Hello("hello-2 dev");
+    }
+
+    @Profile("prod")
+    @Bean("hello-2")
+    public Hello helloProd2() {
+        return new Hello("hello-2 prod");
+    }
+}
