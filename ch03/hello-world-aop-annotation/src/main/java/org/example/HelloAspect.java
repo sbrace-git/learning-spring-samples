177776a311b68950cb7340465755f386bb543676
sbrace <sbrace0822@gmail.com>
2023-02-27 20:36:14 +0800

    ch03 hello-world-aop-annotation @Pointcut

--- ch03/hello-world-aop-annotation/src/main/java/org/example/HelloAspect.java
+++ ch03/hello-world-aop-annotation/src/main/java/org/example/HelloAspect.java
@@ -3,13 +3,19 @@ package org.example;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class HelloAspect {
 
-    @Before("target(org.example.Hello) && args(words)")
+    @Pointcut("target(org.example.Hello) && args(words)")
+    private void pointCut(StringBuffer words) {
+
+    }
+
+    @Before("pointCut(words)")
     public void addWords(StringBuffer words) {
         words.append(" Welcome to Spring! ");
     }
