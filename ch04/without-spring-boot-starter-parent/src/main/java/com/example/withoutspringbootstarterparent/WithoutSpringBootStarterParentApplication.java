6deb3e9e31eb516920df864adb9cf9c823d60640
sbrace <sbrace0822@gmail.com>
2023-02-28 21:36:58 +0800

    ch04 without-spring-boot-starter-parent complete

---
+++ ch04/without-spring-boot-starter-parent/src/main/java/com/example/withoutspringbootstarterparent/WithoutSpringBootStarterParentApplication.java
@@ -0,0 +1,13 @@
+package com.example.withoutspringbootstarterparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithoutSpringBootStarterParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WithoutSpringBootStarterParentApplication.class, args);
+    }
+
+}
---
+++ ch04/without-spring-boot-starter-parent/src/test/java/com/example/withoutspringbootstarterparent/WithoutSpringBootStarterParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.withoutspringbootstarterparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithoutSpringBootStarterParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
