c7b27f83a1fdd232483c16d1996fcc3c3c9a45f1
sbrace <sbrace0822@gmail.com>
2023-03-05 23:20:00 +0800

    ch05 binary-tea add EchoController

---
+++ ch05/binary-tea/src/main/java/learning/spring/binarytea/controller/EchoController.java
@@ -0,0 +1,15 @@
+package learning.spring.binarytea.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/echo")
+public class EchoController {
+
+    @GetMapping
+    public String echo(String echo) {
+        return echo;
+    }
+}
