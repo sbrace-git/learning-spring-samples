a7d9993fff4711b3775a0475832fd8ca0bf19182
sbrace <sbrace0822@gmail.com>
2023-03-07 00:09:36 +0800

    ch05 binary-tea metrics http.client.requests

--- ch05/binary-tea/src/main/java/learning/spring/binarytea/controller/EchoController.java
+++ ch05/binary-tea/src/main/java/learning/spring/binarytea/controller/EchoController.java
@@ -4,14 +4,28 @@ import io.micrometer.core.annotation.Timed;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/echo")
 public class EchoController {
+
+    private final RestTemplate restTemplate;
+
+
+    public EchoController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     @GetMapping
     @Timed(extraTags = {"region", "cn-shanghai-1"}, percentiles = 0.99D)
     public String echo(String echo) {
         return echo;
     }
+
+    @GetMapping("/restTemplate")
+    public String restTemplate(String echo) {
+        return restTemplate.getForObject("http://localhost:8080/echo?echo=hello", String.class);
+    }
+
 }
