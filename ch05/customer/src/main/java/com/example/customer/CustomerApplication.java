e4331937fdd1205f502908207a93502f4909a149
sbrace <sbrace0822@gmail.com>
2023-03-08 00:17:05 +0800

    ch05 customer test ApplicationRunner

--- ch05/customer/src/main/java/com/example/customer/CustomerApplication.java
+++ ch05/customer/src/main/java/com/example/customer/CustomerApplication.java
@@ -1,11 +1,15 @@
 package com.example.customer;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.util.NumberUtils;
 import org.springframework.util.StringUtils;
+
+import java.util.List;
 
 @Slf4j
 @SpringBootApplication
@@ -16,10 +20,38 @@ public class CustomerApplication {
     }
 
 
+    /*
+        --wait=3,--key=value,Hello
+    */
+
     @Bean
     public CommandLineRunner run() {
+        // 共传入了3个参数。分别是--wait=3,--key=value,Hello
         return args -> log.info("共传入了{}个参数。分别是{}",
                 args.length, StringUtils.arrayToCommaDelimitedString(args));
+    }
+
+    /*
+            还没开门，先等3秒
+            其他参数:Hello
+    */
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return args -> {
+            boolean needWait = args.containsOption("wait");
+            if (!needWait) {
+                log.info("如果门没开就不用等了");
+                return;
+            }
+            List<String> waitSeconds = args.getOptionValues("wait");
+            if (!waitSeconds.isEmpty()) {
+                Integer seconds = NumberUtils.parseNumber(waitSeconds.get(0), Integer.class);
+                log.info("还没开门，先等{}秒", seconds);
+                Thread.sleep(seconds * 1000);
+            }
+            log.info("其他参数:{}", StringUtils.collectionToCommaDelimitedString(args.getNonOptionArgs()));
+        };
     }
 
 }
