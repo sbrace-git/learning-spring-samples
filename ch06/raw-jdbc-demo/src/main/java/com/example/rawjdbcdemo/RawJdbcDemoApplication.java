c76548314d51b05a883e7385e42f10089af9bfb0
sbrace <sbrace0822@gmail.com>
2023-03-09 23:22:37 +0800

    ch06 raw-jdbc-demo test jdbc

--- ch06/raw-jdbc-demo/src/main/java/com/example/rawjdbcdemo/RawJdbcDemoApplication.java
+++ ch06/raw-jdbc-demo/src/main/java/com/example/rawjdbcdemo/RawJdbcDemoApplication.java
@@ -1,13 +1,43 @@
 package com.example.rawjdbcdemo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+@Slf4j
 @SpringBootApplication
 public class RawJdbcDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RawJdbcDemoApplication.class, args);
+
+        try (Connection connection = DriverManager.getConnection("jdbc:h2:mem:test_db");
+             Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery("select x from system_range(1,10)");
+            while (resultSet.next()) {
+                log.info("取值:{}", resultSet.getInt(1));
+            }
+        } catch (Exception e) {
+            log.error("出错啦", e);
+        }
+
+        /*
+            取值:1
+            取值:2
+            取值:3
+            取值:4
+            取值:5
+            取值:6
+            取值:7
+            取值:8
+            取值:9
+            取值:10
+         */
     }
 
 }
