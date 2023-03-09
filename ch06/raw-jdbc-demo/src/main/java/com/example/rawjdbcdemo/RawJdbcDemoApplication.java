27994cca51bcb5a674824eed9645cc5809404983
sbrace <sbrace0822@gmail.com>
2023-03-09 23:17:54 +0800

    ch06 raw-jdbc-demo init

---
+++ ch06/raw-jdbc-demo/src/main/java/com/example/rawjdbcdemo/RawJdbcDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.rawjdbcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RawJdbcDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RawJdbcDemoApplication.class, args);
+    }
+
+}
---
+++ ch06/raw-jdbc-demo/src/test/java/com/example/rawjdbcdemo/RawJdbcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rawjdbcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RawJdbcDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
