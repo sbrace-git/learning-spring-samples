b4b337916828617d0a064d53e4f4df6422511dff
sbrace <sbrace0822@gmail.com>
2023-03-09 23:27:49 +0800

    ch06 raw-jdbc-demo CommandLineRunner

--- ch06/raw-jdbc-demo/src/main/java/com/example/rawjdbcdemo/RawJdbcDemoApplication.java
+++ ch06/raw-jdbc-demo/src/main/java/com/example/rawjdbcdemo/RawJdbcDemoApplication.java
@@ -1,8 +1,10 @@
 package com.example.rawjdbcdemo;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -13,20 +15,12 @@ import java.sql.Statement;
 @SpringBootApplication
 public class RawJdbcDemoApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         SpringApplication.run(RawJdbcDemoApplication.class, args);
+    }
 
-        try (Connection connection = DriverManager.getConnection("jdbc:h2:mem:test_db");
-             Statement statement = connection.createStatement()) {
-            ResultSet resultSet = statement.executeQuery("select x from system_range(1,10)");
-            while (resultSet.next()) {
-                log.info("取值:{}", resultSet.getInt(1));
-            }
-        } catch (Exception e) {
-            log.error("出错啦", e);
-        }
 
-        /*
+    /*
             取值:1
             取值:2
             取值:3
@@ -38,6 +32,21 @@ public class RawJdbcDemoApplication {
             取值:9
             取值:10
          */
+
+    @Bean
+    public CommandLineRunner testJdbc() {
+        return (args) -> {
+            Class.forName("org.h2.Driver");
+            try (Connection connection = DriverManager.getConnection("jdbc:h2:mem:test_db");
+                 Statement statement = connection.createStatement()) {
+                ResultSet resultSet = statement.executeQuery("select x from system_range(1,10)");
+                while (resultSet.next()) {
+                    log.info("取值:{}", resultSet.getInt(1));
+                }
+            } catch (Exception e) {
+                log.error("出错啦", e);
+            }
+        };
     }
 
 }
