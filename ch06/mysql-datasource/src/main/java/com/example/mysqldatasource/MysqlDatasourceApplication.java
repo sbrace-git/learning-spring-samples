5c41087a27e929c5d4db1e9a512111a0e71c365c
sbrace <sbrace0822@gmail.com>
2023-03-14 18:31:23 +0800

    ch06 mysql-datasource config HikariDataSource

--- ch06/mysql-datasource/src/main/java/com/example/mysqldatasource/MysqlDatasourceApplication.java
+++ ch06/mysql-datasource/src/main/java/com/example/mysqldatasource/MysqlDatasourceApplication.java
@@ -1,7 +1,13 @@
 package com.example.mysqldatasource;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class MysqlDatasourceApplication {
@@ -10,4 +16,14 @@ public class MysqlDatasourceApplication {
         SpringApplication.run(MysqlDatasourceApplication.class, args);
     }
 
+    @Bean("dataSource")
+    @ConfigurationProperties("spring.datasource.hikari")
+    public DataSource getDataSource(DataSourceProperties dataSourceProperties) {
+        HikariDataSource hikariDataSource = new HikariDataSource();
+        hikariDataSource.setJdbcUrl(dataSourceProperties.getUrl());
+        hikariDataSource.setUsername(dataSourceProperties.getUsername());
+        hikariDataSource.setPassword(dataSourceProperties.getPassword());
+        hikariDataSource.setDriverClassName(dataSourceProperties.getDriverClassName());
+        return hikariDataSource;
+    }
 }
