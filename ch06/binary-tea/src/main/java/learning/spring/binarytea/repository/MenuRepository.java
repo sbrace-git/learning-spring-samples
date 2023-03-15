83902e94c9cc483250d883d1184489a2f2f65c3c
sbrace <sbrace0822@gmail.com>
2023-03-16 01:41:06 +0800

    ch06 binary-tea jdbc insert delete

--- ch06/binary-tea/src/main/java/learning/spring/binarytea/repository/MenuRepository.java
+++ ch06/binary-tea/src/main/java/learning/spring/binarytea/repository/MenuRepository.java
@@ -3,11 +3,14 @@ package learning.spring.binarytea.repository;
 import learning.spring.binarytea.model.MenuItem;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
+import java.sql.PreparedStatement;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 @Repository
 public class MenuRepository {
@@ -21,7 +24,7 @@ public class MenuRepository {
         return jdbcTemplate.queryForObject("select count(*) from t_menu", Long.class);
     }
 
-    public MenuItem queryFotItem(Long id) {
+    public MenuItem queryForItem(Long id) {
         return jdbcTemplate.queryForObject("select * from t_menu where id = ?", rowMapper(), id);
     }
 
@@ -38,5 +41,35 @@ public class MenuRepository {
 
     public List<MenuItem> queryAllItems() {
         return jdbcTemplate.query("select * from t_menu", rowMapper());
+    }
+
+    public static final String INSERT_SQL =
+            "insert into t_menu (name, size, price, create_time, update_time) values (?, ?, ?, now(), now())";
+
+    public int insertItem(MenuItem item) {
+        return jdbcTemplate.update(INSERT_SQL, item.getName(), item.getSize(),
+                item.getPrice().multiply(BigDecimal.valueOf(100)).longValue());
+    }
+
+    public int insertItemAndFillId(MenuItem item) {
+        GeneratedKeyHolder generatedKeyHolder = new GeneratedKeyHolder();
+
+        int affected = jdbcTemplate.update(con -> {
+            PreparedStatement preparedStatement = con.prepareStatement(INSERT_SQL, PreparedStatement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, item.getName());
+            preparedStatement.setString(2, item.getSize());
+            preparedStatement.setLong(3, item.getPrice().multiply(BigDecimal.valueOf(100)).longValue());
+            return preparedStatement;
+        }, generatedKeyHolder);
+
+        if (affected == 1) {
+            item.setId(Objects.requireNonNull(generatedKeyHolder.getKey()).longValue());
+        }
+
+        return affected;
+    }
+
+    public int deleteItem(Long id) {
+        return jdbcTemplate.update("delete from t_menu where id = ?", id);
     }
 }
--- ch06/binary-tea/src/test/java/learning/spring/binarytea/repository/MenuRepositoryTest.java
+++ ch06/binary-tea/src/test/java/learning/spring/binarytea/repository/MenuRepositoryTest.java
@@ -1,7 +1,10 @@
 package learning.spring.binarytea.repository;
 
 import learning.spring.binarytea.model.MenuItem;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,16 +14,19 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MenuRepositoryTest {
     @Autowired
     private MenuRepository menuRepository;
 
     @Test
+    @Order(0)
     void testCountMenuItems() {
         assertEquals(2, menuRepository.countMenuItems());
     }
 
     @Test
+    @Order(0)
     void testQueryAllItems() {
         List<MenuItem> menuItems = menuRepository.queryAllItems();
         assertNotNull(menuItems);
@@ -37,8 +43,35 @@ public class MenuRepositoryTest {
     }
 
     @Test
+    @Order(0)
     void testQueryForItem() {
-        MenuItem menuItem = menuRepository.queryFotItem(1L);
+        MenuItem menuItem = menuRepository.queryForItem(1L);
         assertItem(menuItem, 1L, "Java咖啡", "中杯", BigDecimal.valueOf(10.00d));
+    }
+
+    @Test
+    @Order(1)
+    void testInsertItem() {
+        MenuItem item = MenuItem.builder()
+                .name("Go橙汁").size("中杯")
+                .price(BigDecimal.valueOf(12.00d))
+                .build();
+        assertEquals(1, menuRepository.insertItem(item));
+        assertNull(item.getId());
+        MenuItem queryItem = menuRepository.queryForItem(3L);
+        assertItem(queryItem, 3L, "Go橙汁", "中杯", BigDecimal.valueOf(12.00));
+
+        // fill id
+        assertEquals(1, menuRepository.insertItemAndFillId(item));
+        assertNotNull(item.getId());
+        queryItem = menuRepository.queryForItem(item.getId());
+        assertItem(queryItem, 4L, "Go橙汁", "中杯", BigDecimal.valueOf(12.00));
+    }
+
+    @Test
+    @Order(2)
+    void testDelete() {
+        assertEquals(1, menuRepository.deleteItem(3L));
+        assertEquals(1, menuRepository.deleteItem(2L));
     }
 }
