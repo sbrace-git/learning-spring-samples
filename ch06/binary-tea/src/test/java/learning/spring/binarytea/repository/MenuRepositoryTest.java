b2a54e2ca2b6e7ce06fa499cd136e675e8bdb87b
sbrace <sbrace0822@gmail.com>
2023-03-16 00:55:31 +0800

    ch06 binary-tea add test MenuRepository

--- ch06/binary-tea/src/test/java/learning/spring/binarytea/repository/MenuRepositoryTest.java
+++ ch06/binary-tea/src/test/java/learning/spring/binarytea/repository/MenuRepositoryTest.java
@@ -1,9 +1,14 @@
 package learning.spring.binarytea.repository;
 
-import org.junit.jupiter.api.Assertions;
+import learning.spring.binarytea.model.MenuItem;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class MenuRepositoryTest {
@@ -12,6 +17,28 @@ public class MenuRepositoryTest {
 
     @Test
     void testCountMenuItems() {
-        Assertions.assertEquals(2,menuRepository.countMenuItems());
+        assertEquals(2, menuRepository.countMenuItems());
+    }
+
+    @Test
+    void testQueryAllItems() {
+        List<MenuItem> menuItems = menuRepository.queryAllItems();
+        assertNotNull(menuItems);
+        assertFalse(menuItems.isEmpty());
+        assertEquals(2, menuItems.size());
+    }
+
+    private void assertItem(MenuItem item, Long id, String name, String size, BigDecimal price) {
+        assertNotNull(item);
+        assertEquals(id, item.getId());
+        assertEquals(name, item.getName());
+        assertEquals(size, item.getSize());
+        assertEquals(price, item.getPrice());
+    }
+
+    @Test
+    void testQueryForItem() {
+        MenuItem menuItem = menuRepository.queryFotItem(1L);
+        assertItem(menuItem, 1L, "Java咖啡", "中杯", BigDecimal.valueOf(10.00d));
     }
 }
